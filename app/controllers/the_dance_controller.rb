889d37c9360b82a61cd7d50a6e5c132275fd849b
episod <taylorsingletary@gmail.com>
2010-03-01 09:59:35 -0800

    Mastering the dance

--- app/controllers/the_dance_controller.rb
+++ app/controllers/the_dance_controller.rb
@@ -32,26 +32,52 @@ class TheDanceController < ApplicationController
                                 :service_provider_id => @service_provider.id}      
     end
     
+    if @request_token.callback_confirmed?
+      GhostTrap.trap! :callback_confirmed, "The service provider confirmed the oauth_callback"
+    else
+      GhostTrap.trap! :callback_confirmed, "The service provider did not confirm the oauth_callback"
+    end
+    
     # Now send the user off to authorization
-    send_to_authorization
+    send_to_authorization(@request_token)
   end
 
   def process_callback
+    GhostTrap.trap! :oauth_callback, "OAuth callback hit"
+    request_token = params[:oauth_token]
+    request_token_secret = session[request_token]
+    @service_provider = ServiceProvider.find params[:service_provider_id]
+    if request_token_secret
+      @request_token = OAuth::RequestToken.from_hash(@service_provider.to_oauth_consumer, { :oauth_token => request_token, :oauth_token_secret => request_token_secret})
+      @access_token get_access_token(@service_provider, @request_token)
+      if @
+        flash[:notice] = "#{params[:id].humanize} was successfully connected to your account"
+        go_back
+      else
+        flash[:error] = "An error happened, please try connecting to #{@service_provider.label} again"
+        redirect_to :action => "index"
+      end
+    end    
   end
   
   def process_out_of_band
   end
   
-  def send_to_authorization
-    url = @request_token.authorize_url
+  def send_to_authorization(request_token)
+    url = request_token.authorize_url
     GhostTrap.trap! :authorize_url, "Redirecting to #{url}"
-    redirect_to @request_token.authorize_url
-  end
-
-  def get_access_token
+    redirect_to url
   end
 
   def take_a_bow
+  end
+
+
+  def get_access_token(service_provider, request_token)
+    
+  end
+  
+  def store_access_token(service_provider, access_token)
   end
 
 end
