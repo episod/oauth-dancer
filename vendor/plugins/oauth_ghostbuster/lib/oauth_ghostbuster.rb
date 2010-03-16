ce81d34dfb017a5e9cf481fe75b5f6df502b3b4f
episod <taylorsingletary@gmail.com>
2010-03-16 08:59:15 -0700

    Backported a fix from Ruby's net/http library to not swallow socket errors

--- vendor/plugins/oauth_ghostbuster/lib/oauth_ghostbuster.rb
+++ vendor/plugins/oauth_ghostbuster/lib/oauth_ghostbuster.rb
@@ -219,3 +219,39 @@ module OAuth
     end
   end
 end
+
+module Net
+  class HTTP
+    def request(req, body = nil, &block)  # :yield: +response+
+      unless started?
+        start {
+          req['connection'] ||= 'close'
+          return request(req, body, &block)
+        }
+      end
+      if proxy_user()
+        unless use_ssl?
+          req.proxy_basic_auth proxy_user(), proxy_pass()
+        end
+      end
+
+      req.set_body_internal body
+      begin_transport req
+        req.exec @socket, @curr_http_version, edit_path(req.path)
+        begin
+          res = HTTPResponse.read_new(@socket)
+        end while res.kind_of?(HTTPContinue)
+        res.reading_body(@socket, req.response_body_permitted?) {
+          yield res if block_given?
+        }
+      end_transport req, res
+
+      res
+    rescue => exception
+      D "Conn close because of error #{exception}"
+      # backporting fix - TS
+      @socket.close if @socket and not @socket.closed?
+      raise exception
+    end
+  end
+end
